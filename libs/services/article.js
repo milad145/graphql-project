4a1ad5e81a24336ca17a67dcde94767ae239488d
Aslani <aslani@sc.spinn.ai>
2025-01-19 10:43:21 +0330

    paginate

--- libs/services/article.js
+++ libs/services/article.js
@@ -1,11 +1,17 @@
 import Article from '../entities/article/query.js'
 
-const articleService = new Article();
+const articleDB = new Article();
 
-export const findArticles = async () => {
-    return articleService.find({}, {}, {})
+export const findArticles = async (page, limit) => {
+    const skip = (page - 1) * limit;
+    let result = await articleDB.find({}, {}, {limit, skip, populate: true})
+    let count = await articleDB.count({})
+    return {
+        result,
+        paginate: {count, limit, page, pages: Math.ceil(count / limit)}
+    }
 }
 
 export const getArticle = async (_id) => {
-    return articleService.get(_id, {}, {})
+    return articleDB.get(_id, {}, {})
 }
