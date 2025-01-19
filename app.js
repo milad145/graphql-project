51dc23a3df8237cee839b0ede514e0113a659852
Aslani <aslani@sc.spinn.ai>
2025-01-19 10:46:29 +0330

    rename endpoint

--- app.js
+++ app.js
@@ -83,14 +83,14 @@ const initiateExpress = () => {
 
     const app = express();
 
-    app.all("/milad", createHandler({
+    app.all("/main", createHandler({
         schema,
         rootValue
     }))
 
     app.get('/', (_req, res) => {
         res.type('html');
-        res.end(ruruHTML({endpoint: '/milad'}));
+        res.end(ruruHTML({endpoint: '/main'}));
     });
 
     app.listen(4000, () => console.log("Running on port 4000"));
