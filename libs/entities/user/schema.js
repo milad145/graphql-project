23134520e726266edf98d12b1e54a0f8fcba70be
milad145 <miladaslani1991@gmail.com>
2025-01-23 16:36:07 +0330

    add virtual to schema

--- libs/entities/user/schema.js
+++ libs/entities/user/schema.js
@@ -1,15 +1,15 @@
 import {Schema} from 'mongoose';
 
 const schema = new Schema({
-    name: { type: String, required: true },
-    age: { type: Number, required: true },
-    address: { type: String, required: true },
-    admin: { type: Boolean, default: 0 },
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-}, {timestamps: true, collection: "user"})
+    name: {type: String, required: true},
+    age: {type: Number, required: true},
+    address: {type: String, required: true},
+    admin: {type: Boolean, default: 0},
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+}, {timestamps: true, collection: "user", toJSON: {virtuals: true}})
 
-schema.virtual("article", {
+schema.virtual("articles", {
     ref: "article",
     localField: "_id",
     foreignField: "user"
