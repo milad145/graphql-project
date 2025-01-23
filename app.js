956578e78419a325a50e5e412e444163fd6b1772
milad145 <miladaslani1991@gmail.com>
2025-01-23 17:19:12 +0330

    import config

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
+import config from './libs/config/index.js'
 import {initiateExpress} from "./libs/init.js";
 
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb://0.0.0.0:27017/graphql");
+        const conn = await mongoose.connect(config.database);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
 
-        initiateExpress()
+        initiateExpress(config)
     } catch (err) {
         console.log(err);
         process.exit(1);
