41b7a0c33e888c4312dd4585fea1ce0de9b5801c
milad145 <miladaslani1991@gmail.com>
2025-01-23 17:09:53 +0330

    root values

---
+++ libs/graphql/rootValue.js
@@ -0,0 +1,34 @@
+import {findArticles, getArticle} from "../services/article.js";
+import {findUsers, getUser} from "../services/user.js";
+import {findComments} from "../services/comment.js";
+
+export const rootValue = {
+    articles: (args) => {
+        let {page, limit} = args
+        page = page || 1;
+        limit = limit || 10
+        return findArticles(page, limit)
+    },
+    article: async (args) => getArticle(args._id),
+    users: ({page, limit}) => {
+        page = page || 1;
+        limit = limit || 10
+        return findUsers(page, limit)
+    },
+    user: async ({_id}) => {
+        let user = await getUser(_id)
+        if (!user) {
+            let err = new Error()
+            err.messageCode = 1001
+            err.responseCode = 404
+            err.message = 'user not founded!!'
+            throw err
+        }
+        return user
+    },
+    comments: async ({article, page, limit}) => {
+        page = page || 1;
+        limit = limit || 10;
+        return findComments(article, page, limit)
+    }
+}
