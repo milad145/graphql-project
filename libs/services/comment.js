88ab231cdbc22ceef53f9445a58f61f63eb440a7
milad145 <miladaslani1991@gmail.com>
2025-01-23 21:13:18 +0330

    remove unused parameters

--- libs/services/comment.js
+++ libs/services/comment.js
@@ -2,7 +2,7 @@ import Comment from '../entities/comment/query.js'
 
 const commentDB = new Comment();
 
-export const getArticleComments = async (article, page, limit) => {
+export const getArticleComments = async (article) => {
     return await commentDB.find({article, approved: true}, {}, {skip: 0, limit: 3})
 }
 
