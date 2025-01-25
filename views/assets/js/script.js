e8662f23383655d228b026e1fff20309351aedaf
milad145 <miladaslani1991@gmail.com>
2025-01-26 00:43:58 +0330

    crud queries (mutation)

--- views/assets/js/script.js
+++ views/assets/js/script.js
@@ -60,4 +60,95 @@ async function getUsers() {
     }
 }
 
-(async () => await getUsers())()
+async function addArticle() {
+    try {
+        let body = {
+            query: `
+        mutation publishArticle($title: String!,$body: String!){
+          addArticle(title:$title, body: $body){
+            _id
+            title
+            body
+            createdAt
+            updatedAt
+            user{
+              _id
+              name
+            }
+            comments{
+              _id
+              comment
+              user{
+                _id
+                name
+              }
+            }
+          }
+        }
+        `,
+            variables: {
+                "title": "test title",
+                "body": "Lorem ipsume is a ..."
+            }
+        }
+        const {data} = await request(body);
+        const article = data.addArticle
+        console.log(article)
+        await updateArticle(article._id)
+        console.log('article updated!')
+        await deleteArticle(article._id)
+        console.log('article deleted!')
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+async function updateArticle(_id) {
+    try {
+        let body = {
+            query: `
+                mutation updateArticle($_id: String!, $title: String, $body: String){
+                  updateArticle(_id:$_id, title:$title, body:$body){
+                    _id
+                    title
+                    body
+                    createdAt
+                    updatedAt
+                  }
+                }
+        `,
+            variables: {
+                _id,
+                "title": "test title (updated!)",
+                "body": "Lorem ipsume is a ... (updated!)"
+            }
+        }
+        const {data} = await request(body);
+        console.log(data)
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+async function deleteArticle(_id) {
+    try {
+        let body = {
+            query: `
+                mutation deleteArticle($_id: String!){
+                  deleteArticle(_id:$_id)
+                }
+        `,
+            variables: {
+                _id,
+                "title": "test title",
+                "body": "Lorem ipsume is a ..."
+            }
+        }
+        const {data} = await request(body);
+        console.log(data)
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+(async () => await addArticle())()
