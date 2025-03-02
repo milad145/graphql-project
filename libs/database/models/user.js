cbed8e2f071ee2612cfd0cb8d9a34ef678656b96
Aslani <aslani@sc.spinn.ai>
2025-03-02 14:39:05 +0330

    save user password as hash and compare it for login

--- libs/database/models/user.js
+++ libs/database/models/user.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const modelName = 'user';
 const schema = new Schema({
@@ -9,6 +10,24 @@ const schema = new Schema({
     email: {type: String, unique: true, required: true},
     password: {type: String, required: true}
 }, {timestamps: true, collection: modelName, toJSON: {virtuals: true}})
+
+schema.pre("save", async function (next) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+
+})
+schema.methods.comparePassword = async function (password) {
+    try {
+        return await bcrypt.compare(password, this.password)
+    } catch (e) {
+        throw e
+    }
+}
 
 schema.virtual("articles", {
     ref: "article",
