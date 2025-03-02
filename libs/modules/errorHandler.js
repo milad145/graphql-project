ec63349eacc240722c17dd5b4d5ccf59cad0e7ba
Aslani <aslani@sc.spinn.ai>
2025-03-02 14:36:26 +0330

    manage errors and modify proper error message

---
+++ libs/modules/errorHandler.js
@@ -0,0 +1,25 @@
+export const errorCode = (code) => {
+    let error = {};
+
+    // default
+    error[400] = {message: "Bad Request.", code: 400};
+    error[401] = {message: "Unauthorized! Please login", code: 401};
+    error[403] = {message: "Access Denied!", code: 403};
+    error[404] = {message: "Not Found", code: 404};
+    error[410] = {message: "Expired!", code: 410};
+
+    // user > 2000
+    error[2001] = {message: "You are registered already!", code: 409};
+    error[2002] = {message: "َUser not registered yet!", code: 404};
+    error[2003] = {message: "Wrong username or password.", code: 403};
+    // user > 2000
+    error[2101] = {message: "Article not found!", code: 404};
+
+    let response = error[code] || {};
+    let err = new Error();
+    err["responseCode"] = response.code || 500;
+    err["message"] = response.message || "";
+    err["messageCode"] = code || "";
+
+    return err;
+};
