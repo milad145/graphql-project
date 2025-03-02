0156006afbce05732ed668fbcf6ca079236f48b3
Aslani <aslani@sc.spinn.ai>
2025-03-02 14:39:33 +0330

    add secret and expire time for jwt tokens

--- libs/config/development.js
+++ libs/config/development.js
@@ -1,5 +1,9 @@
 export default {
     database: process.env.DB_CONFIG,
     environment: 'development',
-    port: process.env.PORT || 3300
+    port: process.env.PORT || 3300,
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+    accessTokenExpireTime: "2h",
+    refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+    refreshTokenExpireTime: "1d",
 };
--- libs/config/production.js
+++ libs/config/production.js
@@ -1,5 +1,15 @@
+import TokenGenerator from '../modules/tokenGenerator.js'
+
+const tokenGenerator = new TokenGenerator(64, true, true, true, false)
+process.env.ACCESS_TOKEN_SECRET = tokenGenerator.newToken()
+process.env.REFRESH_TOKEN_SECRET = tokenGenerator.newToken()
+
 export default {
     database: process.env.DB_CONFIG,
     environment: 'development',
-    port: process.env.PORT || 3300
+    port: process.env.PORT || 3300,
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+    accessTokenExpireTime: "2h",
+    refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+    refreshTokenExpireTime: "1d",
 };
