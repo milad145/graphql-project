87ffe4a59990f18234f2e128e8435773abfa62ec
Aslani <aslani@sc.spinn.ai>
2025-03-03 15:57:13 +0330

    import context from file and export it for usage

--- libs/graphql/index.js
+++ libs/graphql/index.js
@@ -1,5 +1,6 @@
 import {typeDefs} from './schema.js'
 import {resolvers} from "./resolver.js";
+import context from './context.js'
 
 
-export {typeDefs, resolvers}
+export {typeDefs, resolvers, context}
