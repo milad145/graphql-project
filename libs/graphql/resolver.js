9c212c06f064fb804295b60c7166986702de0b82
Aslani <aslani@sc.spinn.ai>
2025-03-03 15:57:41 +0330

    check user is login

--- libs/graphql/resolver.js
+++ libs/graphql/resolver.js
@@ -8,32 +8,48 @@ import {
 } from "../services/article.js";
 import {findUsers, getUser, register, login} from "../services/user.js";
 import {getArticleComments, getComment, getUserComments} from "../services/comment.js";
+import {errorCode} from "../modules/errorHandler.js";
 
 export const resolvers = {
     Query: {
-        article: async (_, {_id}) => getArticle(_id),
-        articles: (_, {page, limit}) => {
+        article: async (_, {_id}, {user}) => {
+            await isLogin(user)
+            return getArticle(_id)
+        },
+        articles: async (_, {page, limit}, {user}) => {
+            await isLogin(user)
             page = page || 1;
             limit = limit || 10
             return findArticles({}, page, limit)
         },
-        user: async (_, {_id}) => await getUser(_id),
-        users: (_, {page, limit}) => {
+        user: async (_, {_id}, {user}) => {
+            await isLogin(user)
+            return await getUser(_id)
+        },
+        users: async (_, {page, limit}, {user}) => {
+            await isLogin(user)
             page = page || 1;
             limit = limit || 10
             return findUsers(page, limit)
         },
-        comment: async (_, {_id}) => await getComment(_id)
+        comment: async (_, {_id}, {user}) => {
+            await isLogin(user)
+            return await getComment(_id)
+        }
     },
     Mutation: {
-        addArticle: async (_, {title, body}) => {
-            let user = "5c46c0d169720e4bc0d05cbe"
-            return await addArticle(user, title, body)
+        addArticle: async (_, {title, body}, {user}) => {
+            await isLogin(user)
+            return await addArticle(user._id, title, body)
         },
-        updateArticle: async (_, {_id, title, body}) => {
+        updateArticle: async (_, {_id, title, body}, {user}) => {
+            await isLogin(user)
             return await updateArticle(_id, title, body)
         },
-        deleteArticle: async (_, {_id}) => await deleteArticle(_id),
+        deleteArticle: async (_, {_id}, {user}) => {
+            await isLogin(user)
+            return await deleteArticle(_id)
+        },
         register: async (_, {name, age, address, email, password}) => {
             return await register(name, age, address, email, password)
         },
@@ -53,4 +69,11 @@ export const resolvers = {
         user: async ({user}) => await getUser(user),
         article: async ({article}) => await getArticle(article)
     }
+}
+
+const isLogin = async (user) => {
+    if (user && user._id)
+        return true
+    else
+        throw errorCode(401)
 }
